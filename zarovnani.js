d7534795e5d0b7508dc4aa00b42710b59f79e73a
Tomáš Glozar <tglozar@gmail.com>
2016-09-11 19:28:52 +0200

    Opraveny otravne chyby.

--- zarovnani.js
+++ zarovnani.js
@@ -1,12 +1,12 @@
 function onBodyLoad() {
-      if (document.getElementById('text').scrollHeight < (window.innerHeight - 130)) {
-        document.getElementById('text').style.height = window.innerHeight - 130 + "px";
+      if (document.getElementById('text').scrollHeight < (window.outerHeight - 130)) {
+        document.getElementById('text').style.height = window.outerHeight - 130 + "px";
         document.getElementById('menu').style.height = document.getElementById('text').scrollHeight + "px";
       }
       else {
         document.getElementById('menu').style.height = document.getElementById('text').scrollHeight + "px";
       }
-      document.getElementById("text").style.width = (window.innerWidth - 280) + "px";
+      document.getElementById("text").style.width = (window.outerWidth - 280) + "px";
 }
 
 window.onresize = onBodyLoad;
